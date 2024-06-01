53241f7eaa3691927b14a41deea0e5ae48cf9e8a
cto <support@imaginestack.io>
2024-05-31 23:05:06 -0700

    Commit New Base

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ansible-galaxy collection install -r ./collections/requirements.yml
-ansible-playbook -i inventory/hosts.ini playbooks/site.yml
--- reset.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ansible-playbook -i inventory/hosts.ini playbooks/reset.yml
